e1feead77353dce464ec4605ade021c2a32ef742
ellierossuck <48635728+ellierossuck@users.noreply.github.com>
2019-11-10 18:29:27 +0000

    Updates

---
+++ precache-manifest.4de4b25eae71af64f7fe23c04284f95b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b0d214103763ba2c9f735db3ac1eade",
+    "url": "/es-impact-calculator/index.html"
+  },
+  {
+    "revision": "5079ec9622fc454a9854",
+    "url": "/es-impact-calculator/static/css/main.bb69082e.chunk.css"
+  },
+  {
+    "revision": "6d47094b4a2d878fe3e1",
+    "url": "/es-impact-calculator/static/js/2.2a1fb280.chunk.js"
+  },
+  {
+    "revision": "5079ec9622fc454a9854",
+    "url": "/es-impact-calculator/static/js/main.af7d3a75.chunk.js"
+  },
+  {
+    "revision": "ab033cf0f1dc114e8b4f",
+    "url": "/es-impact-calculator/static/js/runtime~main.3c60403e.js"
+  },
+  {
+    "revision": "aa7ad41167ff201106785b70d6b28435",
+    "url": "/es-impact-calculator/static/media/beluga.aa7ad411.jpg"
+  },
+  {
+    "revision": "40c5ba9a463d2d0fc32dd36128a8eb7a",
+    "url": "/es-impact-calculator/static/media/cars.40c5ba9a.jpg"
+  },
+  {
+    "revision": "300174f558e246063b0631a49e46bdf9",
+    "url": "/es-impact-calculator/static/media/header.300174f5.jpg"
+  },
+  {
+    "revision": "09338ed60e9ec3398b1761893e058f5a",
+    "url": "/es-impact-calculator/static/media/trees.09338ed6.jpg"
+  },
+  {
+    "revision": "af469000edf3a2efb2059707327d363e",
+    "url": "/es-impact-calculator/static/media/vegan.af469000.jpg"
+  }
+]);
